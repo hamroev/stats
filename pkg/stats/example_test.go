ff224a4fc48bdc957f9b8242d1224343715bf2b7
Sodiqjon Hamroev <hamroev.sodikjon@gmail.com>
2020-09-29 09:39:26 +0500

    Сделал задачу по расчету стредней и суммы по категориям

--- pkg/stats/example_test.go
+++ pkg/stats/example_test.go
@@ -1,37 +1,48 @@
 package stats
 
+import (
+	"fmt"
+
+	"github.com/hamroev/bank/v2/pkg/types"
+)
+
 func ExampleAvg() {
 	payments := []types.Payment{
 		{
 			ID:       2,
 			Amount:   5_00,
 			Category: "Cat1",
+			Status:   "OK",
 		},
 		{
 			ID:       1,
 			Amount:   8_00,
 			Category: "Cat1",
+			Status:   "OK",
 		},
 		{
 			ID:       3,
 			Amount:   7_00,
 			Category: "Cat2",
+			Status:   "OK",
 		},
 		{
 			ID:       4,
 			Amount:   9_00,
 			Category: "Cat2",
+			Status:   "FAIL",
 		},
 		{
 			ID:       5,
 			Amount:   -3_00,
 			Category: "Cat3",
+			Status:   "OK",
 		},
 	}
 
 	fmt.Println(Avg(payments))
 
-	//Output: 520
+	//Output: 425
 }
 
 func ExampleTotalInCategory() {
@@ -40,30 +51,35 @@ func ExampleTotalInCategory() {
 			ID:       2,
 			Amount:   5_00,
 			Category: "Cat1",
+			Status:   "OK",
 		},
 		{
 			ID:       1,
 			Amount:   8_00,
 			Category: "Cat1",
+			Status:   "OK",
 		},
 		{
 			ID:       3,
 			Amount:   7_00,
 			Category: "Cat2",
+			Status:   "FAIL",
 		},
 		{
 			ID:       4,
 			Amount:   9_00,
 			Category: "Cat2",
+			Status:   "OK",
 		},
 		{
 			ID:       5,
 			Amount:   -3_00,
 			Category: "Cat3",
+			Status:   "OK",
 		},
 	}
 
 	fmt.Println(TotalInCategory(payments, "Cat2"))
 
-	//Output: 1600
+	//Output: 900
 }
